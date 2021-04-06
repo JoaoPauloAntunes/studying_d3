c697573048da091569e8f51d8cce080d05349c1e
João Paulo Antunes <jpantunesdesouza@gmail.com>
2021-04-06 15:59:33 -0300

    feat: Using express library instead of http library to create server

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const http = require('http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200; // OK
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("teste");
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.listen(3000);
