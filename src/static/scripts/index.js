19344c094f7e1e5872cc75f505638627add25fb6
João Paulo Antunes <jpantunesdesouza@gmail.com>
2021-04-07 14:38:57 -0300

    feat: Add map create with leaflet library

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,4 +1,11 @@
-d3.selectAll("p").style("color", function(d, i) {
-    return i % 2 ? "red" : "blue";
-  });
+// d3.selectAll("p").style("color", function(d, i) {
+//     return i % 2 ? "red" : "blue";
+//   });
   
+var map = L.map(document.getElementById('mapDIV'), {
+  center: [-20.1438, -44.1301],
+  zoom: 15
+});
+
+var basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {});
+basemap.addTo(map);
