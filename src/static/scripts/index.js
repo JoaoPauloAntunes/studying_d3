040617146fa38d15f486aa35fed1d2770970ece7
João Paulo Antunes <jpantunesdesouza@gmail.com>
2021-04-08 14:47:25 -0300

    Add files

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -9,3 +9,26 @@ var map = L.map(document.getElementById('mapDIV'), {
 
 var basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {});
 basemap.addTo(map);
+
+var favorabilityMarker = L.marker(
+  [-20.1438, -44.1301], 
+  {title: 'Coletor 8'}
+);
+favorabilityMarker.bindPopup(`
+  <div class="marker-popup">
+    <p><strong>Descrição: </strong>Coletor 8</p>
+    <p><strong>Favorabilidade: </strong>7.1390832579354</p>
+    <p><strong>Latitude: </strong>-55.02501944</p>
+    <p><strong>Longitude: </strong>-29.63505</p>
+  </div>
+`);
+favorabilityMarker.addTo(map);	
+
+  var theMarkers = L.geoJSON(markers);
+theMarkers.addTo(map);
+
+/* 
+layerGroup = L.layerGroup(theMarkers);
+var sliderControl = L.control.sliderControl({layer:layerGroup});
+map.addControl(sliderControl);
+sliderControl.startSlider(); */
